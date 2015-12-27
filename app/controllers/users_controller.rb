ac49bf6b7380711cc32c9822898f6e9d3c7670bd
jojikobayashi <joji2402@gmail.com>
2015-12-27 11:49:53 +0000

    create edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update] 
   
-  def show 
+  def show
    @user = User.find(params[:id])
   end
   
@@ -17,11 +18,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    unless current_user != @user
+     redirect_to root_path
+    end
+  end
 
+  def update
+    unless current_user != @user
+     redirect_to root_path
+    end
+    if @user.update(user_params)
+      redirect_to root_path , notice: '変更を保存しました'
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
